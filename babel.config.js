ed1ca35a39101de87f32d548467b9995e7bc1312
Noah Weis <njdweis@gmail.com>
2025-04-18 00:04:46 -0700

    fixed plugin error due to babel update

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,15 @@
-const path = require('path');
-
 module.exports = function (api) {
   api.cache(true);
 
   const isWeb = process.env.EXPO_PLATFORM === 'web';
 
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      !isWeb && 'nativewind/babel', // skip nativewind for web
-      require.resolve('expo-router/babel'),
+    presets: [
+      ['babel-preset-expo', { jsxImportSource: 'nativewind' }],
+      !isWeb && 'nativewind/babel',
     ].filter(Boolean),
+
+    plugins: [
+    ],
   };
 };
