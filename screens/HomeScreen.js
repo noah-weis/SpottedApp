f12f8bf59dacd58c2e6e1b44d1ea18d3a3cf6d93
Noah Weis <njdweis@gmail.com>
2025-04-27 19:18:10 -0700

    removed boilerplate and typescript

--- screens/HomeScreen.js
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function HomeScreen() {
-  return (
-    <View className="flex-1 justify-center items-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">Spotted 👀</Text>
-    </View>
-  );
-}
